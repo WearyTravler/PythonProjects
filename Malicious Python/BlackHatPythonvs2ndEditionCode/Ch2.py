1cc59cb93ee7e7e7d56fd8138322f249d2604c79
WearyTravler <limbo_shell@protonmail.com>
2025-09-06 16:45:26 -0500

    more bytes

--- Malicious Python/BlackHatPythonvs2ndEditionCode/Ch2.py
+++ Malicious Python/BlackHatPythonvs2ndEditionCode/Ch2.py
@@ -54,12 +54,12 @@ def udp_client(target_host, target_port):
   client.connect((target_host, target_port))
 
 # send data
-  client.sendto("AAABBBCCC",(target_host, target_port))
+  client.sendto(b"AAABBBCCC",(target_host, target_port))
 
 # receive some data
   data, addr = client.recvfrom(4096)
 
-  print(data)
+  print(data        )
 
 
 #-----------------------------------------------------------------
