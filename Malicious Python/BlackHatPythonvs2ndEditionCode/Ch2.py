08ae6cae7c28e9f234db9382340d6c8638516634
WearyTravler <limbo_shell@protonmail.com>
2025-09-06 16:02:26 -0500

    some extra comments

--- Malicious Python/BlackHatPythonvs2ndEditionCode/Ch2.py
+++ Malicious Python/BlackHatPythonvs2ndEditionCode/Ch2.py
@@ -8,6 +8,12 @@ target_port = 80
 # socket obj
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+## AF_INET = using ipv4 addressing or hostnaming
+## SOCK_STREAM = indicates a TCP client
+
+# making a lot of assumptions...
+
+
 # connect to the client
 client.connect((target_host, target_port))
 
