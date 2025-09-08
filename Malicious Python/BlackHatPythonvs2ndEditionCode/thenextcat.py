4a96110f694ea514f5056d501858aab5fb9a6c7e
WearyTravler <limbo_shell@protonmail.com>
2025-09-07 23:29:08 -0500

    thenextcat

---
+++ Malicious Python/BlackHatPythonvs2ndEditionCode/thenextcat.py
@@ -0,0 +1,11 @@
+import sys,socket,getopt,threading,subprocess
+
+
+# def global var
+listen      = False
+command     = False
+upload      = False
+execute     = ""
+target      = ""
+upload_dest = ""
+port        = 0
